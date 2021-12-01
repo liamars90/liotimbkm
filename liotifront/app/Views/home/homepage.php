ef7d8b327d34c56e71578d45079d0f6ce18ac4ea
William <william.ar90@outlook.com>
2021-12-02 01:40:23 +0700

    Add Landing Page

---
+++ liotifront/app/Views/home/homepage.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Lioti - Lancar IoT</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="assets/css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+            <div class="container px-5">
+                <a class="navbar-brand fw-bold" href="#page-top">Lioti - Lancar IoT</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="bi-list"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                        <li class="nav-item"><a class="nav-link me-lg-3" href="#features">Fitur</a></li>
+                        <li class="nav-item"><a class="nav-link me-lg-3" href="#aboutus">Tentang Kami</a></li>
+                    </ul>
+                    <a class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" href="https://learning.lioti.online">
+                        <span class="d-flex align-items-center">
+                            <i class="bi bi-arrow-bar-right me-2"></i>
+                            <span class="small">Mulai belajar</span>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <!-- Mashead header-->
+        <header class="masthead">
+            <div class="container px-5">
+                <div class="row gx-5">
+                    <div class="col-lg-6 order-lg-1">
+                        <img class="img-fluid" src="assets/img/education.png" alt="education" />
+                        
+                    </div>
+                    <div class="col-lg-6 order-lg-0">
+                        <!-- Mashead text and app badges-->
+                        <div class="mb-lg-0 text-center text-lg-start">
+                            <h1 class="display-1 lh-1 mb-3">Kursus IoT dengan nyaman dan menyeluruh</h1>
+                            <p class="lead fw-normal text-muted mb-5">Belajar IoT secara komprehensif dan nyaman melalui online dengan materi kursus yang dibuat oleh para profesional di bidang IoT </p>
+                            <div class="d-flex flex-column flex-lg-row align-items-center ">
+                                
+                                <button class="btn btn-lg btn-primary px-3 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
+                                    <span class="d-flex align-items-center">
+                                        <i class="bi-chat-text-fill me-2 d-none d-sm-block"></i>
+                                        <span class="small">Daftar Kursus Sekarang juga</span>
+                                    </span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </header>
+        
+        <!-- App features section-->
+        <section id="features">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center">
+                    <div class="col-lg-12 order-lg-1 mb-5 mb-lg-0">
+                        <div class="container-fluid px-5">
+                            <div class="row">
+                                <div class="col-md-12 mb-5 text-center">
+                                    <h2 class="fw-bold lh-1 font-alt mb-1">Mengapa memilih belajar di Lioti?</h2>
+                                </div>
+                            </div>
+                            <div class="row gx-5">
+                                <div class="col-md-6 mb-5">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-people icon-feature text-gradient d-block mb-3"></i>
+                                        <h3 class="font-alt">Pelatih Profesional</h3>
+                                        <p class="text-muted mb-0">Kamu akan belajar bersama dengan pelatih profesional di bidangnya </p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 mb-5">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-phone icon-feature text-gradient d-block mb-3"></i>
+                                        <h3 class="font-alt">Fleksibel</h3>
+                                        <p class="text-muted mb-0">Belajar di mana saja dan kapan saja tergantung waktu yang kamu mau</p>
+                                    </div>
+                                </div>
+                                
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6 mb-5 mb-md-0">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-stack icon-feature text-gradient d-block mb-3"></i>
+                                        <h3 class="font-alt">Materi Terstruktur</h3>
+                                        <p class="text-muted mb-0">Seluruh materi dibuat oleh orang-orang berpengalaman dalam bidang IoT</p>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <!-- Feature item-->
+                                    <div class="text-center">
+                                        <i class="bi-patch-check icon-feature text-gradient d-block mb-3"></i>
+                                        <h3 class="font-alt">Sertifikat Online</h3>
+                                        <p class="text-muted mb-0">Setelah menyelesaikan kursus kamu akan mendapatkan e-certificate </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+        <!-- Basic features section-->
+        <section class="bg-light" id="aboutus">
+            <div class="container px-5">
+                <div class="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+                    <div class="col-12 col-lg-6">
+                        <h2 class="display-4 lh-1 mb-4 d-none d-sm-none d-md-none d-lg-block">Tentang Kami</h2>
+                        <h2 class="text-center display-4 lh-1 mb-4 d-block d-sm-block d-md-block d-lg-none d-xl-none d-xxl-none">Tentang Kami</h2>
+                        <p class="lead fw-normal text-muted mb-5 mb-lg-0">
+                            Lioti merupakan startup yang bergerak di bidang pendidikan khususnya Internet of Things. Kami memiliki tujuan untuk menyediakan kursus yang
+                            disukai oleh berbagai orang sehingga membuat mereka termotivasi untuk terus belajar. Kami menjamin setiap materi yang disediakan telah melalui proses riset oleh para profesional di bidang IoT
+                            
+                        </p>
+                    </div>
+                    <div class="col-sm-12 col-md-6">
+                        <div class="px-sm-0">
+                            
+                            <img class="img-fluid" src="assets/img/ourteam.png" alt="ourteam" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+        <section class="bg-gradient-primary-to-secondary" id="download">
+            <div class="container px-5">
+                <h2 class="text-center text-white font-alt mb-4">Sudah Mendaftar?</h2>
+                <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
+                    <a class="btn btn-dark btn-lg rounded-pill px-3 mb-2 mb-lg-0" href="https://learning.lioti.online">
+                        <span class="d-flex align-items-center">
+                            <i class="bi bi-arrow-bar-right me-2"></i>
+                            <span class="small">Mulai belajar Sekarang</span>
+                        </span>
+                    </a>
+                </div>
+            </div>
+        </section>
+        <!-- Footer-->
+        <footer class="bg-black text-center py-5">
+            <div class="container px-5">
+                <div class="text-white-50 small">
+                    <div class="mb-2">&copy; Lioti - Lancar IoT 2021. All Rights Reserved.</div>
+                    
+                </div>
+            </div>
+        </footer>
+        <!-- Feedback Modal-->
+        <div class="modal fade" id="feedbackModal" tabindex="-1" aria-labelledby="feedbackModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header bg-gradient-primary-to-secondary p-4">
+                        <h5 class="modal-title font-alt text-white" id="feedbackModalLabel">Kami akan menghubungi kamu secepatnya</h5>
+                        <button class="btn-close btn-close-white" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body border-0 p-4">
+                        <form id="contactForm" class="needs-validation" novalidate>
+                            <!-- Name input-->
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="name" type="text" placeholder="Masukan nama anda..." required />
+                                <label for="name">Nama lengkap</label>
+                                <div class="invalid-feedback">Nama lengkap wajib diisi</div>
+                            </div>
+                            <!-- Email address input-->
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="email" type="email" placeholder="nama@example.com" required />
+                                <label for="email">Alamat Email</label>
+                                <div class="invalid-feedback">Alamat email wajib diisi.</div>
+                                <div class="invalid-feedback">Email tidak valid.</div>
+                            </div>
+                            <!-- Phone number input-->
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="phone" type="tel" placeholder="0812xxxxxx" required />
+                                <label for="phone">Nomor HP</label>
+                                <div class="invalid-feedback">Nomor HP wajib diisi</div>
+                            </div>
+                            <!-- Message input-->
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" id="message" type="text" placeholder="Tuliskan pesan anda..." style="height: 10rem" required></textarea>
+                                <label for="message">Pesan Anda</label>
+                                <div class="invalid-feedback">Pesan wajib diisi</div>
+                            </div>
+                            
+                            <!-- Submit Button-->
+                            <div class="d-grid"><button class="btn btn-primary rounded-pill btn-lg" id="submitButton" type="submit">Submit</button></div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="assets/js/scripts.js"></script>
+
+    </body>
+</html>
